cdbea4f5e67668b0efe5de57b2884a784271b568
Giuseppe <giu.sene@gmail.com>
2021-11-30 16:58:48 +0100

    feat: aggiunta funzione login con memorizzazione like e dislike

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 import { loadGenres } from './apis.js';
+import { checkUserLogin } from './login.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     loadGenres();
+    checkUserLogin();
 }
 )
